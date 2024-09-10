6eea88adcbd775f81157185da28084c86d1d8c83
Viacheslav <slavavilhov@gmail.com>
2024-09-10 21:28:50 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
-import { refreshUser } from './redux/auth/authOps';
+import { refreshUser } from './redux/auth/operations';
 import { fetchContacts } from './redux/contacts/operations';
 import { selectIsRefreshing } from './redux/auth/selectors';
 import Layout from './components/Layout/Layout';
